1dd0d78f1cad15726d31f35d877dbf985feea734
BryanGK <bryangordonkrauss@gmail.com>
2021-04-16 08:25:39 -0700

    App display to browser

---
+++ API/Services/StockService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace API.Services
+{
+    public interface IStockService
+    {
+        Task<StockModel> GetStock();
+    }
+
+    public class StockService : IStockService
+    {
+        private readonly HttpClient _client;
+
+        public StockService(IApiHelper apiHelper)
+        {
+            _client = apiHelper.InitializeClient();
+        }
+
+        public  Task<StockModel> GetStock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
