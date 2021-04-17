40bcfa3d82d8dfccc52f69103d9ee3183b5440af
BryanGK <bryangordonkrauss@gmail.com>
2021-04-17 15:33:26 -0700

    Creates login form

--- API/client/src/Pages/Login.js
+++ API/client/src/Pages/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Styles/Login.css';
 
 function Login() {
     return (
         <div>
             <h1>Login</h1>
+            <div className="container login-form-container">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="row login-form">
+                            <form>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Username">
+                                </input>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Password">
+                                </input>
+                                <Link to="/home"><button
+                                    className="btn btn-primary"
+                                    type="submit">
+                                    Login
+                                    </button></Link>
+                                <button
+                                    className="btn btn-secondary"
+                                    type="submit">
+                                    Sign Up
+                                    </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
