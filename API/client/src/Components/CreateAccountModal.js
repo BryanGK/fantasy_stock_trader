1f1a1fe9b90531a617c66e27f1405b3412ad08dc
BryanGK <bryangordonkrauss@gmail.com>
2021-04-18 10:52:57 -0700

    Creates create account modal

---
+++ API/client/src/Components/CreateAccountModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CreateAccountModal() {
+    return (
+        <div className="modal-dialog modal-dialog-centered">
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            This is the body of the modal.
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateAccountModal;
--- API/client/src/Pages/Login.js
+++ API/client/src/Pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CreateAccountModal from '../Components/CreateAccountModal';
 import '../Styles/Login.css';
 
 function Login() {
@@ -28,10 +29,33 @@ function Login() {
                                     </button></Link>
                                 <button
                                     className="btn btn-secondary"
-                                    type="submit">
+                                    type="button"
+                                    data-target="#staticBackdrop"
+                                    data-toggle="modal">
+
                                     Sign Up
                                     </button>
                             </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal fade" id="staticBackdrop" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                This is the body of the modal.
+                        </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <Link to="/home"><button type="button" className="btn btn-primary" data-dismiss="modal">Create Account</button></Link>
+                            </div>
                         </div>
                     </div>
                 </div>
