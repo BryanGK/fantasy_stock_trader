fe1a275d60692694d78b06de4af809da52a2b618
BryanGK <bryangordonkrauss@gmail.com>
2021-04-19 12:02:01 -0700

    Adds search bar to Home.js

--- API/client/src/Pages/Home.js
+++ API/client/src/Pages/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import SummaryChart from '../Components/SummaryChart';
+import StockSearch from '../Components/StockSearch';
 import '../Styles/Home.css';
 
 function Home() {
+    const handleSearch = e => {
+        e.preventDefault();
+        console.log("search");
+    }
+
     return (
         <div className="home">
             <h1 className="home-header">Home</h1>
             <div className="container chart-container">
                 <SummaryChart />
+            </div>
+            <div className="stock-search-container">
+                <StockSearch
+                    handleSearch={handleSearch}
+                />
             </div>
         </div>
     )
