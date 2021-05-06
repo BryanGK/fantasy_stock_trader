58833cfa3f8ee03aae774e4f0a3e4502cb4b1dcc
BryanGK <bryangordonkrauss@gmail.com>
2021-05-06 09:09:27 -0700

    Adds basic form to buystockmodal

--- API/client/src/Components/BuyStockModal.js
+++ API/client/src/Components/BuyStockModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import '../Styles/BuyStockModal.css';
 
 function BuyStockModal({ handleModalClose, stockQuote }) {
     if (!stockQuote)
@@ -7,18 +8,88 @@ function BuyStockModal({ handleModalClose, stockQuote }) {
 
     return (
         <div>
-            <Form>
+            <Form className="buy-stock-form">
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        Buy Stock
+                        Buy {stockQuote.companyName} Stock
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Buy {stockQuote.companyName} Stock</h4>
-                    <p>Filler text</p>
+                    <Form.Group as={Row}>
+                        <Form.Label
+                            htmlFor="current-price"
+                            column sm={4}
+                        >
+                            Current Price:
+                        </Form.Label>
+                        <Col sm={3} className="current-price">
+                            <Form.Control
+                                id="current-price"
+                                placeholder={`$${stockQuote.latestPrice}`}
+                                disabled
+                            >
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label
+                            htmlFor="quantity"
+                            column sm={4}
+                        >
+                            How Many:
+                        </Form.Label>
+                        <Col sm={3} className="quantity">
+                            <Form.Control
+                                id="quantity"
+                                placeholder="0"
+                            >
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label
+                            htmlFor="total-price"
+                            column sm={4}
+                        >
+                            Total Price:
+                        </Form.Label>
+                        <Col sm={3} className="total-price">
+                            <Form.Control
+                                id="total-price"
+                                placeholder={`${stockQuote.latestPrice}`}
+                                disabled
+                            >
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label
+                            htmlFor="funds"
+                            column sm={4}
+                        >
+                            Available Funds:
+                        </Form.Label>
+                        <Col sm={3} className="funds">
+                            <Form.Control
+                                id="funds"
+                                placeholder="0"
+                            >
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={handleModalClose}>Close</Button>
+                    <Button
+                        variant="primary"
+                    >
+                        Buy Stock
+                        </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={handleModalClose}
+                    >
+                        Close
+                        </Button>
                 </Modal.Footer>
             </Form>
         </div>
