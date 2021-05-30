6003490b770ee3c63c11805ac13470c10ec0b01d
BryanGK <bryangordonkrauss@gmail.com>
2021-05-30 07:16:13 -0700

    Adds table to Report

--- API/client/src/Pages/Report.js
+++ API/client/src/Pages/Report.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 import '../Styles/Report.css';
 
 function Report() {
     return (
         <div>
             <h1 className="report-header">Report</h1>
+            <div className="container transaction-list">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </Table>
+            </div>
         </div>
     )
 }
