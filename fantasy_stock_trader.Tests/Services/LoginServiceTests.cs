cab94d6025eb6cd9dc31be2da686b5edd150fb59
BryanGK <bryangordonkrauss@gmail.com>
2021-06-12 08:18:13 -0700

    Adds tests to logintests

--- fantasy_stock_trader.Tests/Services/LoginServiceTests.cs
+++ fantasy_stock_trader.Tests/Services/LoginServiceTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using NUnit.Framework;
+
 namespace fantasy_stock_trader.Tests.Services
 {
+    [TestFixture]
     public class LoginServiceTests
     {
-        public LoginServiceTests()
+       [Test]
+       public void GetUserByName_UserIsNull_ThrowsUserNotFoundException()
         {
+
+        }
+
+        [Test]
+        public void GetUserByName_NotValidPassword_ThrowsUserNotFoundException()
+        {
+
+        }
+
+        [Test]
+        public void GetUserByName_ValidUsernameAndPassword_ReturnsUserSession()
+        {
+
         }
     }
 }
